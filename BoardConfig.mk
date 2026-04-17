4ec95f78cebcbfd12ccd43ffb4000730cbd02280
ozturkmutlu65 <ozturkmutlu65@gmail.com>
2026-04-17 17:07:14 +0300

    sapphire: Welcome NFC by ozturkmutlu65

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -267,6 +267,7 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += n7n n7n_ss
 ODM_MANIFEST_N7N_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 ODM_MANIFEST_N7N_SS_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
